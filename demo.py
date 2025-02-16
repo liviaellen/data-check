05b3c55a01d3ae015017c281795c35bb650bf9c3
liviaellen <38367623+liviaellen@users.noreply.github.com>
2025-02-16 05:54:44 -0800

    test recap

--- demo.py
+++ demo.py
@@ -4,12 +4,12 @@
 
 def circle_area(radius):
     """Returns the area of the circle of given radius"""
-    if radius < 0:
-        return 0
+    # if radius < 0:
+    #     return 0
     area = 3.14 * radius * radius
     return area
 
-# print(help(circle_area))
+print(help(circle_area))
 
 # answer = circle_area(1)
 
